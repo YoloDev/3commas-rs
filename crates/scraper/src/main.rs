8de6f7c8d215a731352deaa2d1d0fbefa74213b5
Aleksander Heintz <alxandr@alxandr.me>
2021-04-25 00:05:03 +0200

    Enable different log formats

--- crates/scraper/src/main.rs
+++ crates/scraper/src/main.rs
@@ -4,6 +4,7 @@ mod server_tracing;
 
 use anyhow::Result;
 use cache::Cache;
+use clap::Clap;
 use decimal_gauge::DecimalGaugeVec;
 use prometheus::{
   core::{AtomicU64, GenericGaugeVec},
@@ -15,6 +16,18 @@ use tide::{Body, Request};
 use tracing_subscriber::EnvFilter;
 
 type U64GaugeVec = GenericGaugeVec<AtomicU64>;
+
+#[derive(Clap, Debug, PartialEq)]
+enum LogFormat {
+  Pretty,
+  Json,
+}
+
+#[derive(Clap, Debug)]
+struct App {
+  #[clap(arg_enum, env = "LOG_FORMAT", default_value = "pretty")]
+  log_format: LogFormat,
+}
 
 #[derive(Clone)]
 struct Gauges {
@@ -43,6 +56,23 @@ fn bot_opts(name: &str, help: &str) -> Opts {
 
 #[async_std::main]
 async fn main() -> Result<()> {
+  let app = App::parse();
+
+  match app.log_format {
+    LogFormat::Pretty => {
+      tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .with_max_level(tracing::Level::INFO)
+        .init();
+    }
+    LogFormat::Json => {
+      tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .json()
+        .init();
+    }
+  }
+
   tracing_subscriber::fmt()
     .with_env_filter(EnvFilter::from_default_env())
     .with_max_level(tracing::Level::INFO)
