81db401b87b5050bb676b8f637aafd7c50ca7e9a
Aleksander Heintz <alxandr@alxandr.me>
2021-11-24 14:40:29 +0100

    refactor: don't use deprecated api

---
+++ crates/client/src/lib.rs
@@ -0,0 +1,56 @@
+mod deals;
+mod errors;
+mod middleware;
+
+pub use deals::{Deals, DealsScope};
+pub use three_commas_types::{Bot, BotStats, Deal, Pair};
+
+use middleware::RequestBuilderExt;
+use std::time::Duration;
+use surf::{http::Result, Client, Config, Url};
+
+#[derive(Clone)]
+pub struct ThreeCommasClient {
+  pub(crate) client: Client,
+}
+
+impl ThreeCommasClient {
+  pub fn new(api_key: impl AsRef<str>, secret: impl AsRef<str>) -> Self {
+    let client: Client = Config::new()
+      .set_base_url(Url::parse("https://api.3commas.io/public/api/").unwrap())
+      .try_into()
+      .unwrap();
+
+    let client = client
+      .with(middleware::TracingRequestLoggerMiddlware)
+      .with(middleware::ApiKeyMiddleware::new(api_key.as_ref()))
+      .with(middleware::SigningMiddleware::new(secret.as_ref()))
+      .with(middleware::ErrorHandlerMiddleware)
+      .with(middleware::Limit::new(2, Duration::from_secs(1)))
+      .with(middleware::Limit::new(30, Duration::from_secs(60)))
+      .with(middleware::TracingPipelineLoggerMiddlware);
+
+    Self { client }
+  }
+
+  pub async fn bots(&self) -> Result<Vec<Bot>> {
+    let req = self.client.get("ver1/bots").signed();
+    self.client.recv_json(req).await
+  }
+
+  pub async fn bot_stats(&self, bot: &Bot) -> Result<BotStats> {
+    let req = self
+      .client
+      .get(format!(
+        "ver1/bots/stats?account_id={}&bot_id={}",
+        bot.account_id(),
+        bot.id()
+      ))
+      .signed();
+    self.client.recv_json(req).await
+  }
+
+  pub fn deals(&self) -> Deals {
+    Deals::new(self.clone())
+  }
+}
