f60400be360825456d10ebfe999ffdf717031e49
Aleksander Heintz <alxandr@alxandr.me>
2021-12-05 01:40:05 +0100

    feat: add support for summary account

--- crates/types/src/account.rs
+++ crates/types/src/account.rs
@@ -1,10 +1,11 @@
 use chrono::{DateTime, Utc};
 use rust_decimal::Decimal;
-use serde::{Deserialize, Serialize};
+use serde::{de, Deserialize, Serialize};
+use std::fmt;
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct Account {
-  pub id: usize,
+  pub id: AccountId,
   pub auto_balance_period: u32,
   pub auto_balance_portfolio_id: Option<u32>,
   pub auto_balance_currency_change_limit: Option<u32>,
@@ -24,8 +25,8 @@ pub struct Account {
   pub bots_tsl_allowed: bool,
   pub gordon_bots_available: bool,
   pub multi_bots_allowed: bool,
-  pub created_at: DateTime<Utc>,
-  pub updated_at: DateTime<Utc>,
+  pub created_at: Option<DateTime<Utc>>,
+  pub updated_at: Option<DateTime<Utc>>,
   pub last_auto_balance: Option<DateTime<Utc>>,
   /// Sell all to USD/BTC possibility
   pub fast_convert_available: bool,
@@ -62,6 +63,13 @@ pub struct Account {
   pub address: Option<String>,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum AccountId {
+  Summary,
+
+  AccountId(u32),
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub enum AutoBalanceMethod {
   #[serde(rename = "time")]
@@ -78,4 +86,91 @@ pub enum MarketType {
 
   #[serde(rename = "futures")]
   Futures,
+}
+
+impl fmt::Display for AccountId {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    match self {
+      Self::Summary => f.write_str("summary"),
+      Self::AccountId(id) => fmt::Display::fmt(id, f),
+    }
+  }
+}
+
+impl Serialize for AccountId {
+  #[inline]
+  fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+  where
+    S: serde::Serializer,
+  {
+    match self {
+      Self::Summary => serializer.serialize_str("summary"),
+      Self::AccountId(id) => id.serialize(serializer),
+    }
+  }
+}
+
+struct AccountIdVisitor;
+impl<'de> de::Visitor<'de> for AccountIdVisitor {
+  type Value = AccountId;
+
+  fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+    formatter.write_str("account id string or number")
+  }
+
+  fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+  where
+    E: de::Error,
+  {
+    match v {
+      "summary" => Ok(AccountId::Summary),
+      _ => Err(E::invalid_value(de::Unexpected::Str(v), &self)),
+    }
+  }
+
+  fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E>
+  where
+    E: de::Error,
+  {
+    match u32::try_from(v) {
+      Ok(v) => self.visit_u32(v),
+      Err(_) => Err(E::invalid_value(de::Unexpected::Unsigned(v), &self)),
+    }
+  }
+
+  fn visit_u32<E>(self, v: u32) -> Result<Self::Value, E>
+  where
+    E: de::Error,
+  {
+    Ok(AccountId::AccountId(v))
+  }
+
+  fn visit_i32<E>(self, v: i32) -> Result<Self::Value, E>
+  where
+    E: de::Error,
+  {
+    match u32::try_from(v) {
+      Ok(v) => self.visit_u32(v),
+      Err(_) => Err(E::invalid_value(de::Unexpected::Signed(v as i64), &self)),
+    }
+  }
+
+  fn visit_i64<E>(self, v: i64) -> Result<Self::Value, E>
+  where
+    E: de::Error,
+  {
+    match u32::try_from(v) {
+      Ok(v) => self.visit_u32(v),
+      Err(_) => Err(E::invalid_value(de::Unexpected::Signed(v), &self)),
+    }
+  }
+}
+
+impl<'de> Deserialize<'de> for AccountId {
+  fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+  where
+    D: serde::Deserializer<'de>,
+  {
+    deserializer.deserialize_any(AccountIdVisitor)
+  }
 }
--- crates/types/src/lib.rs
+++ crates/types/src/lib.rs
@@ -3,7 +3,7 @@ mod bots;
 mod common;
 mod pair;
 
-pub use account::{Account, AutoBalanceMethod, MarketType};
+pub use account::{Account, AccountId, AutoBalanceMethod, MarketType};
 pub use bots::{Bot, BotStats, Deal, DealStatus, TokenValues};
 pub use common::*;
 pub use pair::Pair;
