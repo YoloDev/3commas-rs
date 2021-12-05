a41d0cc847e8b2915276026c60b3be4dc9188fbe
Aleksander Heintz <alxandr@alxandr.me>
2021-12-05 01:14:24 +0100

    fix: make fields on account type optional

--- crates/types/src/account.rs
+++ crates/types/src/account.rs
@@ -6,14 +6,14 @@ use serde::{Deserialize, Serialize};
 pub struct Account {
   pub id: usize,
   pub auto_balance_period: u32,
-  pub auto_balance_portfolio_id: u32,
-  pub auto_balance_currency_change_limit: u32,
+  pub auto_balance_portfolio_id: Option<u32>,
+  pub auto_balance_currency_change_limit: Option<u32>,
   pub autobalance_enabled: bool,
   pub hedge_mode_available: bool,
   pub hedge_mode_enabled: bool,
   pub is_locked: bool,
   pub smart_trading_supported: bool,
-  pub available_for_trading: bool,
+  // pub available_for_trading: bool,
   pub stats_supported: bool,
   pub trading_supported: bool,
   pub market_buy_supported: bool,
@@ -26,20 +26,20 @@ pub struct Account {
   pub multi_bots_allowed: bool,
   pub created_at: DateTime<Utc>,
   pub updated_at: DateTime<Utc>,
-  pub last_auto_balance: DateTime<Utc>,
+  pub last_auto_balance: Option<DateTime<Utc>>,
   /// Sell all to USD/BTC possibility
   pub fast_convert_available: bool,
   pub grid_bots_allowed: bool,
   pub api_key_invalid: bool,
   pub deposit_enabled: bool,
-  pub supported_market_types: String,
-  pub api_key: String,
+  pub supported_market_types: Vec<String>,
+  pub api_key: Option<String>,
   pub name: String,
-  pub auto_balance_method: AutoBalanceMethod,
-  pub auto_balance_error: String,
-  pub customer_id: String,
-  pub subaccount_name: String,
-  pub lock_reason: String,
+  pub auto_balance_method: Option<AutoBalanceMethod>,
+  pub auto_balance_error: Option<String>,
+  pub customer_id: Option<String>,
+  pub subaccount_name: Option<String>,
+  pub lock_reason: Option<String>,
   pub btc_amount: Decimal,
   pub usd_amount: Decimal,
   pub day_profit_btc: Decimal,
@@ -59,7 +59,7 @@ pub struct Account {
   pub pretty_display_type: String,
   pub exchange_name: String,
   pub market_code: String,
-  pub address: String,
+  pub address: Option<String>,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
@@ -69,4 +69,13 @@ pub enum AutoBalanceMethod {
 
   #[serde(rename = "currency_change")]
   CurrencyChange,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub enum MarketType {
+  #[serde(rename = "spot")]
+  Spot,
+
+  #[serde(rename = "futures")]
+  Futures,
 }
